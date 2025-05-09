4b04b50e9bcbecb65985dc98d4f13a63399c66e7
Rasel Ahmed <raselwebdev@hotmail.com>
2025-05-10 00:31:01 +0300

    3 problem solved

--- index.ts
+++ index.ts
@@ -1,7 +1,34 @@
-function add(num1: string, num2: boolean) : string{
-    if(num2 === true){
-        console.log(num2)
-    }
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    return input.toLowerCase();
+  } else {
+    return input.toUpperCase();
+  }
 }
+console.log(formatString("Hello"));
+console.log(formatString("Hello", true));
+console.log(formatString("Hello", false));
 
-console.log(add("20",true));
+type Product = { title: string; rating: number };
+function filterByRating(items: Product[]): Product[] {
+  return items.filter((item) => {
+    return item.rating >= 4;
+  });
+}
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+console.log(filterByRating(books));
+
+
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.reduce((acc, currentArray) => [...acc, ...currentArray], []);
+}
+const arrayOfStrings = concatenateArrays<string>(["a", "b"], ["c", "d"], ["e"]);
+const arrayOfNumbers = concatenateArrays<number>([1, 2], [3, 4, 5], [6]);
+
+console.log(arrayOfStrings);
+console.log(arrayOfNumbers);
